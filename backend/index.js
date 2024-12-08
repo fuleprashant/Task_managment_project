58e9f09a5b6c001e4ed601ca9ade041886e8af49
Prashant Fule <fuleprashant2809@gmail.com>
2024-12-08 13:22:39 +0530

    setup frontend with react and tailwind and backend with node and express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("backend start")
+import express from "express";
+
+const app = express();
+// console.log(app)
+
+const port = 4000;
+
+app.get("/start", (req, res) => {
+    res.send("the server has been start")
+})
+app.listen(port, () => {
+    console.log(`the server is run on the browser http://localhost:${port}`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,6 @@
+
+const App = () => {
+  return <div className="tet-2xl bg-red-500 text-blue">this is the setup for the frontend and install the react and tailwind css</div>;
+};
+
+export default App;
